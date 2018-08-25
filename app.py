8cf073543c03aaec32aa57d2898fd6399001b422
Arteau <arteaudemeester@gmail.com>
2018-08-25 17:22:47 +0200

    first commit even though I have been working on this for days because I am stupid

--- app.py
+++ app.py
@@ -1,36 +1,14 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-from data import Richtingen
-import MySQLdb
+
+import sqlite3 as sql
+import sys
+from sqlite3 import  Error
+from flask import g
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from pprint import pprint
 
 app = Flask(__name__)
-
-hostname = 'localhost'
-username = 'root'
-password = ''
-database = 'schooldb'
-
-
-
-
-
-
-Richtingen = Richtingen()
-
-@app.route('/')
-def home():
-    conn = MySQLdb.connect( host=hostname, user=username, passwd=password, db=database)
-    cur = conn.cursor()
-    cur.execute( '''SELECT voornaam FROM leeraars WHERE id = 1''' )
-    data = cur.fetchall()
-    # return render_template('home.html', data = str(data))
-    return str(data)
-
-@app.route('/aanbod')
-def aanbod():
-    return render_template('aanbod.html', richtingen = Richtingen)
-
 
 class RegisterForm(Form):
     name = StringField('Naam', [validators.Length(min=1, max=50)])
@@ -41,18 +19,87 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField('Confirmeer paswoord')
 
+
+# Routes
+@app.route('/')
+def home():
+
+    return render_template('home.html')
+    
+
+@app.route('/aanbod')
+def aanbod():
+    return render_template('aanbod.html')
+
+
+@app.route('/wieiswie')
+def wieiswie():
+    return render_template('wieiswie.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/intranet')
+def intranet():
+
+    return render_template('intranet.html')
+
+
+@app.route('/nieuweleraar',methods=['POST', 'GET'])
+def nieuweleraar():
+    msg = "method is niet post"
+    pprint(vars(request.form['naam']))
+    if request.method == 'POST':
+        try:
+            naam = request.form['naam']
+            voornaam = request.form['voornaam']
+            email = request.form['email']
+            foto = request.form['foto']
+
+            with sql.connect("D:\\School\WebConcepten\webconcepten-flask\school.db") as conn:
+                cur = conn.cursor()
+                cur.execute("INSERT INTO leraars (naam, voornaam, email, foto) VALUES (?,?,?,?)", (naam, voornaam, email, foto))
+                conn.commit()
+                msg = "Leraar toevoegen succesvol!"
+        except:
+            print('in post except')
+            conn.rollback()
+            msg = "Er is iets fout gegaan bij het toevoegen van een leraar aan de database."
+
+        finally:
+            naam = request.form['naam']
+            msg = str(naam)
+
+            return render_template("resultaat.html", msg = msg)
+            conn.close()
+
+
+
+
+
+
+
+
 @app.route('/registreer', methods=['GET', 'POST'])
 def registreren():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        return render_template('registreer.html',form=form)
+        name = form.name.data
+        email = form.email.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+
+        flash("Je bent succesvol geregistreerd en kan nu inloggen.")
+
+        redirect(url_for('home'))
     return render_template('registreer.html',form=form)
 
 
 
-
-
-
-
+# Run
 if __name__ == '__main__':
+    app.secret_key='secretkey123'
     app.run(debug=True)
