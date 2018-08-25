5b6711c42b07215a9c14933d60a68044fe468559
Arteau <arteaudemeester@gmail.com>
2018-08-25 22:51:56 +0200

    last commit before the deadline, tried session

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-import logging
 from logging.handlers import RotatingFileHandler
+# from flask_session import Session
 import sqlite3 as sql
 import sys
-from sqlite3 import  Error
+from sqlite3 import Error
 from flask import g
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
@@ -11,6 +11,11 @@ from pprint import pprint
 from base64 import b64encode
 
 app = Flask(__name__)
+
+# SESSION_TYPE = "redis"
+# app.config.from_object(__name__)
+# Session(app)
+
 
 class RegisterForm(Form):
     name = StringField('Naam', [validators.Length(min=1, max=50)])
@@ -25,28 +30,6 @@ DATABASE = 'D:/School/WebConcepten/webconcepten-flask/school.db'
 
 def connect_db():
     return sql.connect(DATABASE)
-
-
-# Routes
-@app.route('/')
-def home():
-
-    return render_template('home.html')
-    
-
-@app.route('/aanbod')
-def aanbod():
-    return render_template('aanbod.html')
-
-
-@app.route('/wieiswie')
-def wieiswie():
-    return render_template('wieiswie.html')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 
 def getLeraarData(c):
     c.execute("SELECT id FROM leraren")
@@ -86,6 +69,27 @@ def getKlasData(c):
     klasData = {'id' : klasId, 'namen' : klasNamen, 'numCode' : klasNumCodes}
     return klasData 
 
+
+# Routes
+@app.route('/')
+def home():
+
+    return render_template('home.html')
+    
+@app.route('/aanbod')
+def aanbod():
+    return render_template('aanbod.html')
+
+
+@app.route('/wieiswie')
+def wieiswie():
+    return render_template('wieiswie.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/intranet')
 def intranet():
     conn = connect_db()
@@ -98,11 +102,84 @@ def intranet():
     klassenRows = cur.fetchall()
     conn.close()
     return render_template('intranet.html', lerarenRows = lerarenRows, richtingenRows = richtingenRows, klassenRows = klassenRows)
-    # leraren = getLeraarData(cur)
-    # richtingen = getRichtingData(cur)
-    # klassen = getKlasData(cur)
-    # return render_template('intranet.html', leraren = leraren, richtingen = richtingen, klassen = klassen)
 
+@app.route('/verwijderrichting/<int:entry_id>', methods=['POST', 'GET'])
+def verwijderrichting(entry_id):
+    if request.method == 'POST':      
+        conn = connect_db()
+        conn.row_factory = sql.Row
+        cur = conn.cursor()
+        verwijder_id = int(entry_id)
+        cur.execute("DELETE FROM richtingen WHERE id=?", [verwijder_id])
+        conn.commit()
+        msg = "Richting succesvol verwijderd."
+        conn.close()
+        return render_template("resultaat.html", msg = msg)
+    else:
+        msg = "Richting is niet verwijderd, fout!"
+        return render_template("resultaat.html", msg = msg)
+
+@app.route('/nieuwerichting', methods=['POST', 'GET'])
+def nieuwerichting():
+    if request.method == 'POST':
+        try:
+            naam = request.form['naam']
+            omschrijving = request.form['omschrijving']
+
+            with connect_db() as conn:
+                cur = conn.cursor()
+                cur.execute("INSERT INTO richtingen (naam, omschrijving) VALUES (?,?)", (naam, omschrijving))
+                conn.commit()
+                msg = "Richting toevoegen succesvol!"
+        except:
+            conn.rollback()
+            msg = "Er is iets fout gegaan bij het toevoegen van een richting aan de database."
+
+        finally:
+
+            return render_template("resultaat.html", msg = msg)
+            conn.close()
+
+@app.route('/verwijderklas/<int:entry_id>', methods=['POST', 'GET'])
+def verwijderklas(entry_id):
+    if request.method == 'POST':      
+        conn = connect_db()
+        conn.row_factory = sql.Row
+        cur = conn.cursor()
+        verwijder_id = int(entry_id)
+        cur.execute("DELETE FROM klassen WHERE id=?", [verwijder_id])
+        conn.commit()
+        msg = "Klas succesvol verwijderd."
+        conn.close()
+        return render_template("resultaat.html", msg = msg)
+    else:
+        msg = "Klas is niet verwijderd, fout!"
+        return render_template("resultaat.html", msg = msg)
+
+@app.route('/nieuweklas', methods=['POST', 'GET'])
+def nieuweklas():
+    if request.method == 'POST':
+        try:
+            naam = request.form['naam']
+            numCode = request.form['numCode']
+            leraar_id = request.form['leraar_id']
+            richting_id = request.form['richting_id']
+
+            with connect_db() as conn:
+                cur = conn.cursor()
+                cur.execute("INSERT INTO klassen (naam, numCode, leraar_id, richting_id) VALUES (?,?,?,?)", (naam, numCode, leraar_id, richting_id))
+                conn.commit()
+                msg = "Klas toevoegen succesvol!"
+        except:
+            conn.rollback()
+            msg = "Er is iets fout gegaan bij het toevoegen van een klas aan de database."
+
+        finally:
+
+            return render_template("resultaat.html", msg = msg)
+            conn.close()
+
+   
 @app.route('/verwijderleraar/<int:entry_id>', methods=['POST', 'GET'])
 def verwijderleraar(entry_id):
     if request.method == 'POST':      
@@ -119,7 +196,6 @@ def verwijderleraar(entry_id):
         msg = "Leraar is niet verwijderd, fout!"
         return render_template("resultaat.html", msg = msg)
 
-
 @app.route('/nieuweleraar', methods=['POST', 'GET'])
 def nieuweleraar():
     if request.method == 'POST':
@@ -135,7 +211,6 @@ def nieuweleraar():
                 conn.commit()
                 msg = "Leraar toevoegen succesvol!"
         except:
-            print('in post except')
             conn.rollback()
             msg = "Er is iets fout gegaan bij het toevoegen van een leraar aan de database."
 
@@ -143,12 +218,6 @@ def nieuweleraar():
 
             return render_template("resultaat.html", msg = msg)
             conn.close()
-
-
-
-
-
-
 
 
 @app.route('/registreer', methods=['GET', 'POST'])
